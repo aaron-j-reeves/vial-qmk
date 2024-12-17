fde6d867a421b192040430bc25b5ef018b076fa5
Aaron <aaron.reeves@corgan.com>
2024-12-16 23:37:17 -0600

    fix

--- keyboards/boardsource/unicorne/config.h
+++ keyboards/boardsource/unicorne/config.h
@@ -21,14 +21,14 @@
 #define POINTING_DEVICE_RIGHT
 
 // If ANALOG_JOYSTICK_AUTO_AXIS is used, then ANALOG_JOYSTICK_AXIS_MIN and ANALOG_JOYSTICK_AXIS_MAX are ignored.
-//#define ANALOG_JOYSTICK_AXIS_MIN              // Sets the lower range to be considered movement. - default 0
-//#define ANALOG_JOYSTICK_AXIS_MAX              // Sets the upper range to be considered movement. - default 1023
-#define ANALOG_JOYSTICK_AUTO_AXIS               // Sets ranges to be considered movement automatically.
+#define ANALOG_JOYSTICK_AXIS_MIN 0            // Sets the lower range to be considered movement. - default 0
+#define ANALOG_JOYSTICK_AXIS_MAX 1000             // Sets the upper range to be considered movement. - default 1023
+//#define ANALOG_JOYSTICK_AUTO_AXIS               // Sets ranges to be considered movement automatically.
 
-#define ANALOG_JOYSTICK_SPEED_MAX 4          // The maximum value used for motion. - default 2
-#define ANALOG_JOYSTICK_SPEED_REGULATOR 25      // The divisor used to slow down movement. (lower makes it faster) - default 20
+#define ANALOG_JOYSTICK_SPEED_MAX 5          // The maximum value used for motion. - default 2
+#define ANALOG_JOYSTICK_SPEED_REGULATOR 30      // The divisor used to slow down movement. (lower makes it faster) - default 20
 #define ANALOG_JOYSTICK_READ_INTERVAL 5         // The interval in milliseconds between reads. - default 10
 
 // By default analog joystick implementation uses x^2 weighting for lever positions. ANALOG_JOYSTICK_WEIGHTS allows to experiment with different configurations that might feel better.
 // E.g. This is weights for ((x-0.4)^3+0.064)/0.282:
-// #define ANALOG_JOYSTICK_WEIGHTS {0,2,4,5,7,8,9,10,12,13,14,15,15,16,17,18,18,19,19,20,20,21,21,21,22,22,22,22,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,24,24,24,24,24,24,25,25,25,26,26,26,27,28,28,29,29,30,31,32,33,34,35,36,37,38,40,41,43,44,46,48,49,51,53,56,58,60,62,65,68,70,73,76,79,82,85,89,92,96,100}
+#define ANALOG_JOYSTICK_WEIGHTS {0,2,4,5,7,8,9,10,12,13,14,15,15,16,17,18,18,19,19,20,20,21,21,21,22,22,22,22,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,24,24,24,24,24,24,25,25,25,26,26,26,27,28,28,29,29,30,31,32,33,34,35,36,37,38,40,41,43,44,46,48,49,51,53,56,58,60,62,65,68,70,73,76,79,82,85,89,92,96,100}
--- keyboards/boardsource/unicorne/keymaps/vial/config.h
+++ keyboards/boardsource/unicorne/keymaps/vial/config.h
@@ -22,10 +22,10 @@
 #define RGB_MATRIX_MAXIMUM_BRIGHTNESS 200                       // limits maximum brightness of LEDs to 180 out of 255. If not defined maximum brightness is set to 255
 #define RGB_MATRIX_DEFAULT_ON true                              // Sets the default enabled state, if none has been set
 #define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_RIVERFLOW            // Sets the default mode, if none has been set
-#define RGB_MATRIX_DEFAULT_HUE 20                               // Sets the default hue value, if none has been set
+#define RGB_MATRIX_DEFAULT_HUE 40                               // Sets the default hue value, if none has been set
 #define RGB_MATRIX_DEFAULT_SAT 255                              // Sets the default saturation value, if none has been set
 #define RGB_MATRIX_DEFAULT_VAL RGB_MATRIX_MAXIMUM_BRIGHTNESS    // Sets the default brightness value, if none has been set
-#define RGB_MATRIX_DEFAULT_SPD 127                              // Sets the default animation speed, if none has been set
+#define RGB_MATRIX_DEFAULT_SPD 100                              // Sets the default animation speed, if none has been set
 
 #define TAPPING_TERM 250
 #define COMBO_TERM 40
